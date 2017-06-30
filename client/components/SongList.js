f100c24dee885720edd3779600f60f3fe1fccf8b
Shubham Gupta <shubhamg@outlook.com>
2017-06-30 16:07:31 +0530

    first

---
+++ client/components/SongList.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import { graphql} from 'react-apollo';
+import { Link } from 'react-router';
+//import query from '../queries/fetchSongs';
+
+class SongList extends Component{
+  onSongDelete(id){
+    this.props.mutate({variables: {id}})
+  }
+  renderSong(){
+      return this.props.data.songs.map(({ id, title }) => {
+        return(
+      <li key ={id} className="collection-item">
+        {title}
+        <i
+          className="material-icons"
+          onClick={() => this.onSongDelete(id)}
+          >
+            delete</i>
+      </li>
+    )
+    })
+  }
+
+  render(){
+    if(this.props.data.loading){ return <div>Loading....</div>}
+
+    return(
+      <div>
+    <ul className="collection">
+      {this.renderSong()}
+    </ul>
+    <Link
+      to="/song/new"
+      className="btn-floating btn-large red right">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+    )
+  }
+}
+
+const mutation = gql `
+  mutation DeleteSong($id: ID){
+    deleteSong(id: $id){
+      id
+    }
+  }
+`;
+
+const query = gql`
+  {
+    songs{
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
---
+++ client/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import {ApolloProvider} from 'react-apollo';
+import SongList from './components/SongList';
+import App from './components/App';
+import SongCreate from './components/SongCreate';
+
+const client = new ApolloClient({});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
